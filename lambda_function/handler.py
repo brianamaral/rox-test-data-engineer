caca4ef484bbd5d6a491924d8a690f55e19ec2dd
brianamaral <brianamaral918@gmail.com>
2021-08-04 15:42:06 -0300

    applied black for code formatting

--- lambda_function/handler.py
+++ lambda_function/handler.py
@@ -3,23 +3,30 @@ from sqlalchemy.engine import Connection
 from pandas import DataFrame
 from os import environ
 
+
 class DbHandler:
     def __init__(self):
         self.connection = self._make_connection()
-    
+
     def _make_connection(self) -> Connection:
-        
-        engine = create_engine("postgresql+psycopg2://{}:{}@{}/{}".format(
-            environ['POSTGRES_USER'],
-            environ['POSTGRES_PASSWORD'],
-            environ['POSTGRES_ADDRES'],
-            environ['POSTGRES_DATABASE']
-            ))
-    
+
+        engine = create_engine(
+            "postgresql+psycopg2://{}:{}@{}/{}".format(
+                environ["POSTGRES_USER"],
+                environ["POSTGRES_PASSWORD"],
+                environ["POSTGRES_ADDRES"],
+                environ["POSTGRES_DATABASE"],
+            )
+        )
+
         return engine.connect()
-    
-    def insert_dataframe(self,data: DataFrame,table: str) -> None:
-        data.to_sql(name=table,con=self.connection,if_exists='append',chunksize=5000,method='multi',index=False)
-        
-        
-        
+
+    def insert_dataframe(self, data: DataFrame, table: str) -> None:
+        data.to_sql(
+            name=table,
+            con=self.connection,
+            if_exists="append",
+            chunksize=5000,
+            method="multi",
+            index=False,
+        )
--- lambda_function/lambda_function.py
+++ lambda_function/lambda_function.py
@@ -2,28 +2,24 @@ from handler import DbHandler
 from data_getter import DataGetter
 
 data_getter = DataGetter()
-                    
+
 handler = DbHandler()
 
-files_and_tables = [{'table':'SalesOrderDetail','file':'sales/Sales.SalesOrderDetail.csv'},
-                    {'table':'SalesOrderHeader','file':'sales/Sales.SalesOrderHeader.csv'},
-                    {'table':'Person','file':'person/Person.Person.csv'},
-                    {'table':'SpecialOfferProduct','file':'sales/Sales.SpecialOfferProduct.csv'},
-                    {'table':'Customer','file':'sales/Sales.Customer.csv'},
-                    {'table':'Product','file':'production/Production.Product.csv'}]
-                    
-                    
-def lambda_handler(event,context):
-    
+files_and_tables = [
+    {"table": "SalesOrderDetail", "file": "sales/Sales.SalesOrderDetail.csv"},
+    {"table": "SalesOrderHeader", "file": "sales/Sales.SalesOrderHeader.csv"},
+    {"table": "Person", "file": "person/Person.Person.csv"},
+    {"table": "SpecialOfferProduct", "file": "sales/Sales.SpecialOfferProduct.csv"},
+    {"table": "Customer", "file": "sales/Sales.Customer.csv"},
+    {"table": "Product", "file": "production/Production.Product.csv"},
+]
+
+
+def lambda_handler(event, context):
+
     for pair in files_and_tables:
-        data = data_getter.create_data_frame(file = pair['file'])
-        handler.insert_dataframe(table=pair['table'],data=data)
+        data = data_getter.create_data_frame(file=pair["file"])
+        handler.insert_dataframe(table=pair["table"], data=data)
         del data
-    
-    return 'sucess'
 
-    
-
-    
-
-    
+    return "sucess"
